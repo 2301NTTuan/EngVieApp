f55e84ec8060106bdeff6e1cf95620ca9e3fc4e7
Trung Tuan <129706740+2301NTTuan@users.noreply.github.com>
2023-11-19 11:14:52 +0700

    Update Dictionary.java

--- src/Dictionary/Dictionary.java
+++ src/Dictionary/Dictionary.java
@@ -1,15 +1,16 @@
 package Dictionary;
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Dictionary {
-    public static final String modifiedList = "src\\Dictionaries\\modifiedList.txt";
-    public static final String deletedList = "src\\Dictionaries\\deletedList.txt";
-    public static final String addedList = "src\\Dictionaries\\addedList.txt";
-    public static final String recentWordList = "src\\Dictionaries\\recentWordList.txt";
-    public static final String userList = "src\\Dictionaries\\userList.txt";
+    public static final String modifiedList = getPath() + "/modifiedList.txt";
+    public static final String deletedList = getPath() + "/deletedList.txt";
+    public static final String addedList = getPath() + "/addedList.txt";
+    public static final String recentWordList = getPath() + "/recentWordList.txt";
+    public static final String userList = getPath() + "/userList.txt";
 
-    public static final String mainDictionary = "src\\Dictionaries\\dictionaries.txt";
+    public static final String mainDictionary = getPath() + "/dictionaries.txt";
     public static ArrayList<Word> wordArray = new ArrayList<>();
 
     public static ArrayList<Word> modifiedArray = new ArrayList<>();
@@ -19,5 +20,9 @@ public class Dictionary {
 
     public static  ArrayList<Word> recentWordArray = new ArrayList<>();
     public static ArrayList<Word> userArray = new ArrayList<>();
+
+    public static String getPath () {
+        return Paths.get("EngVieApp").toAbsolutePath() + "/src/Dictionaries/";
+    }
 
 }
